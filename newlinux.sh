868bb1427a502b950e725fc8fd609edd040a6c9f
dor <32441786+Dor-Dahan@users.noreply.github.com>
2022-12-01 23:24:48 +0200

    Update newlinux.sh

--- newlinux.sh
+++ newlinux.sh
@@ -22,7 +22,7 @@ echo "installing pihole"
 
 sleep 5
 
-sudo docker run -d --name pihole -p 53:53/tcp -p 53:53/udp -p 80:80 -p 443:443 -p 8080:8080 -e TZ="Asia/jerusalem" -e WEBPASSWORD="dordor3110"-v "$(pwd)/etc-pihole/:/etc/pihole/" -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" --dns=127.0.0.1 --dns=1.1.1.1 --restart=unless-stopped pihole/pihole:latest
+sudo docker run -d --name pihole -p 53:53/tcp -p 53:53/udp -p 80:80 -p 443:443 -p 8080:8080 -e TZ="Asia/jerusalem" -e WEBPASSWORD="dordor3110" -v "$(pwd)/etc-pihole/:/etc/pihole/" -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" --dns=127.0.0.1 --dns=1.1.1.1 --restart=unless-stopped pihole/pihole:latest
 
 sleep 5
 
