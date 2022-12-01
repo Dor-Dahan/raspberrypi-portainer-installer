65689408d232cc010f66613597dec21fd35b2f42
dor <32441786+Dor-Dahan@users.noreply.github.com>
2022-12-01 22:16:54 +0200

    Update newlinux.sh

--- newlinux.sh
+++ newlinux.sh
@@ -2,11 +2,9 @@ echo "updating java"
 
 sleep 5
 
-sudo apt-get update -y && sudo apt-get install -y \
-    unzip \
-    wget \
-    default-jre \
-    nginx
+echo "installing docker"
+
+sudo apt install docker.io
 
 sleep 5
 
@@ -16,25 +14,46 @@ curl https://download.argon40.com/argon1.sh | bash
 
 sleep 5
 
-echo "downloading docker script"
-
-curl -fsSL https://get.docker.com -o get-docker.sh
-
-sleep 5
-
-echo "activating docker script"
-
-sleep 5
-
-sudo sh ./get-docker.sh
-
-sleep 5
-
 echo "installing portainer in docker"
 
 sleep 5
 
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+sleep 5
+echo "installing pihole"
+sleep 5
+# https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
+
+docker run -d \
+    --name pihole \
+    -p 53:53/tcp -p 53:53/udp \
+    -p 80:80 \
+    -p 443:443 \
+    -p 8080:8080 \
+    -e TZ="Asia/jerusalem" \
+    -v "$(pwd)/etc-pihole/:/etc/pihole/" \
+    -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
+    --dns=127.0.0.1 --dns=1.1.1.1 \
+    --restart=unless-stopped \
+    pihole/pihole:latest
+
+printf 'Starting up pihole container '
+for i in $(seq 1 20); do
+    if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
+        printf ' OK'
+        echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: https://${IP}/admin/"
+        exit 0
+    else
+        sleep 3
+        printf '.'
+    fi
+
+    if [ $i -eq 20 ] ; then
+        echo -e "\nTimed out waiting for Pi-hole start, consult check your container logs for more info (\`docker logs pihole\`)"
+        exit 1
+    fi
+done;
 
 sleep 5
 
