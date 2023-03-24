36b0612c44babe0a391901b17a2d48c5ae4e85e3
dor <32441786+Dor-Dahan@users.noreply.github.com>
2023-03-24 15:58:06 +0300

    Update newlinux.sh

--- newlinux.sh
+++ newlinux.sh
@@ -14,7 +14,7 @@ echo "installing portainer in docker"
 
 sleep 5
 
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 sleep 5
 
