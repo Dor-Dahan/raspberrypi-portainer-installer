f8cb12b3ab54b3a3b81a0d16adeb0b0fcac6d15f
dor <32441786+Dor-Dahan@users.noreply.github.com>
2023-03-24 15:48:44 +0300

    Update newlinux.sh

--- newlinux.sh
+++ newlinux.sh
@@ -14,31 +14,7 @@ echo "installing portainer in docker"
 
 sleep 5
 
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-sleep 5
-
-echo "installing pihole"
-
-sleep 5
-
-read -p 'set pihole time zone: ' timezone
-
-read -p 'set pihole password: ' password
-
-sleep 5
-
-echo $timezone
-
-echo $password
-
-sleep 5
-
-sudo docker run -d --name pihole -p 53:53/tcp -p 53:53/udp -p 80:80 -p 443:443 -p 8080:8080 -p 67:67/udp -e TZ="$timezone" -e WEBPASSWORD="$password" -v "$(pwd)/etc-pihole/:/etc/pihole/" -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" --dns=127.0.0.1 --dns=1.1.1.1 --restart=unless-stopped pihole/pihole:latest
-
-sleep 5
-
-echo "updating static ip"
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 sleep 5
 
@@ -50,9 +26,4 @@ sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -
 sleep 5
 
 sleep 30
-
-echo "rebooting"
-
-sleep 5
-
-sudo reboot
+ echo "done"
