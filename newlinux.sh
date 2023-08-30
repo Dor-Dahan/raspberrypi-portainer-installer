31cb97488aceab09bf7cafc3c24a7376d2877356
dor <32441786+Dor-Dahan@users.noreply.github.com>
2023-08-30 17:53:57 +0300

    Update newlinux.sh

--- newlinux.sh
+++ newlinux.sh
@@ -4,11 +4,32 @@ where:
         -H|H|-h|h  show this help text
         -A|A|-a|a install all docker & portainer & argon40
         -D|D|-d|d install docker only
-        -P|P|-p|p install portainer only"
+        -P|P|-p|p install portainer only
+        -ip|-IP|ip|IP set static ip address"
 
 for arg in "$@"
 do
         case $arg in
+         -ip|-IP|ip|IP)
+                 echo "set static interface"
+                read static_interface
+                echo "set static ip"
+                read static_ip
+                echo "set static routers"
+                read static_routers
+                echo "set static dns"
+                read static_dns
+                sudo echo " interface $static_interface"  >> /etc/dhcpcd.conf
+                sleep 5
+                sudo echo "static ip_address=$static_ip"  >> /etc/dhcpcd.conf
+                sleep 5
+                sudo echo "static routers=$static_routers"  >> /etc/dhcpcd.conf
+                sleep 5
+                sudo echo "static domain_name_servers=$static_dns"  >> /etc/dhcpcd.conf
+                sleep 10
+                sudo reboot
+                shift
+        ;;
         -d|-D|d|D)
                 sudo apt install docker.io -y
                 shift
@@ -25,16 +46,7 @@ do
                 sleep 5
                 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
                 sleep 5
-                sudo echo "interface wlan0"  >> /etc/dhcpcd.conf
-                sleep 5
-                sudo echo "static ip_address=192.168.7.29/24"  >> /etc/dhcpcd.conf
-                sleep 5
-                sudo echo "static routers=192.168.7.1"  >> /etc/dhcpcd.conf
-                sleep 5
-                sudo echo "static domain_name_servers=192.168.7.29"  >> /etc/dhcpcd.conf
-                sleep 10
-                sudo reboot
-                shift
+                 shift
         ;;
         -h|-H|h|H)
                 shift
